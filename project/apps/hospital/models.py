4fbf6426b6cd09ce8da24ad5c9ec9688ff01019b
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-05-26 12:30:21 -0300

    Se creo formularios y vista de la db en app hospital

--- project/apps/hospital/models.py
+++ project/apps/hospital/models.py
@@ -5,15 +5,24 @@ class Paciente(models.Model):
     apellido = models.CharField(max_length=50)
     fecha_de_nacimiento = models.DateField()
 
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
 class Medico(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     especialidad = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} - {self.especialidad}"
+
 class Turno(models.Model):
     fecha = models.DateTimeField()
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.fecha} - {self.paciente} - {self.medico}"
 
 class Receta(models.Model):
     medicamento = models.CharField(max_length=100)
@@ -21,3 +30,6 @@ class Receta(models.Model):
     instrucciones = models.TextField()
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.paciente} - {self.medico}"
